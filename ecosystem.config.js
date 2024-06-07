10ff4a06e82cdccb4165445a3dc3d61d8f5a0142
Galangnurbudii <galang.utomo@binus.ac.id>
2024-06-08 02:24:13 +0700

    Add ecosystem

---
+++ ecosystem.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  apps : [{
+    script: 'npm start'
+  },
+
+  deploy : {
+    production : {
+      user : 'lders',
+      host : '182.253.47.91',
+      ref  : 'origin/main',
+      repo : 'https://github.com/Arch-Deus/ld-retail.git',
+      path : '/home/lders',
+      'pre-deploy-local': '',
+      'post-deploy' : 'source ~/.nvm/nvm.sh && npm install && npm run build && pm2 reload ecosystem.config.js --env production',
+      'pre-setup': '',
+      'ssh_options': 'ForwardAgent=yes'
+    }
+  }
+};
