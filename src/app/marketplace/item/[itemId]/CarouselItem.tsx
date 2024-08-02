208f188f9848b229092f0371196b78d3f8ec19ad
Robby Dharmawan <dharmawans.robby@gmail.com>
2024-08-02 08:46:54 +0700

    Update carousel size

--- src/app/marketplace/item/[itemId]/CarouselItem.tsx
+++ src/app/marketplace/item/[itemId]/CarouselItem.tsx
@@ -46,17 +46,17 @@ const CarouselItem = ({connection, itemId}: CarouselItemProp) => {
         return (
             <div className='mt-5 p-0 md:p-10'>
                 {/* <div>{JSON.stringify(data)}</div> */}
-                <div className="h-auto">
+                <div className="h-56 md:h-[560px]">
                     <Carousel>
                     {
                         data.data.d.detailItemImage.length > 0 ? 
                         data.data.d.detailItemImage.map((key: any) => (
-                            <Image priority key={key.id} className='w-full h-full' src={connection.host+key.fileName+"?access_token="+connection.token+"&session="+connection.session} width={0} height={0} alt="" sizes="100vw" />
+                            <Image priority key={key.id} className='w-full h-auto' src={connection.host+key.fileName+"?access_token="+connection.token+"&session="+connection.session} width={0} height={0} alt="" sizes="100vw" />
                             // <div>{searchParams.host+key.fileName+"?access_token="+searchParams.token+"&session="+searchParams.session}</div>
                             // <img src={searchParams.host+resultFetchGetDetailItem.d.detailItemImage[0].fileName+"?access_token="+searchParams.token+"&session="+searchParams.session} width={0} height={0} alt="" sizes="100vw" />
                         ))
                         :
-                        <Image className='w-full h-full' src={"/default_item.png"} priority width={0} height={0} alt="" sizes="1000vw" />
+                        <Image className='w-full h-auto' src={"/default_item.png"} priority width={0} height={0} alt="" sizes="1000vw" />
                     }
                     </Carousel>
                 </div>
